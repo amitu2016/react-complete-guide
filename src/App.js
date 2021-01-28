9555f535a27357b78b4b3e5de318ae298b338ed1
amitu2016 <amitu2016@gmail.com>
2021-01-28 20:53:12 +0530

    Using Dynamic components

--- src/App.js
+++ src/App.js
@@ -3,11 +3,32 @@ import './App.css';
 import Person from './Person/Person'
 
 class App extends Component {
+  state = {
+    persons: [
+      {name: "Upadhyay", age: 27},
+      {name: "Cena", age: 22},
+      {name: "Orton", age: 29}
+    ]
+  }
+
+  switchNameHandler = () => {
+    //console.log('Was Clicked');
+    // Dont do this : this.state.persons[0].name = 'Upadhyay';
+    this.setState({persons:  [
+      {name: "Amit", age: 27},
+      {name: "Sumit", age: 22},
+      {name: "Ajay", age: 25}
+    ] })
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hi, I am a React App</h1>
-        <Person />
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person name= {this.state.persons[0].name} age={this.state.persons[0].age}  />
+        <Person name= {this.state.persons[1].name} age={this.state.persons[1].age} >My Hobbies: Gaming</Person>
+        <Person name= {this.state.persons[2].name} age={this.state.persons[2].age} />
       </div>
       
     );
