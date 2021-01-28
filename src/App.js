72a79b4b2ff43e035cb4755d9df3e152b0d0b893
amitu2016 <amitu2016@gmail.com>
2021-01-28 21:35:05 +0530

    Using React Hooks

--- src/App.js
+++ src/App.js
@@ -1,39 +1,40 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Person from './Person/Person'
 
-class App extends Component {
-  state = {
-    persons: [
-      {name: "Upadhyay", age: 27},
-      {name: "Cena", age: 22},
-      {name: "Orton", age: 29}
-    ]
-  }
+const app = props => {
+const [ personState, setPersonState ] = useState({ persons: [
+    {name: "Upadhyay", age: 27},
+    {name: "Cena", age: 22},
+    {name: "Orton", age: 29}
+  ]});
 
-  switchNameHandler = () => {
+  const switchNameHandler = () => {
     //console.log('Was Clicked');
-    // Dont do this : this.state.persons[0].name = 'Upadhyay';
-    this.setState({persons:  [
+    // Dont do this : personState.persons[0].name = 'Upadhyay';
+    setPersonState({persons:  [
       {name: "Amit", age: 27},
       {name: "Sumit", age: 22},
       {name: "Ajay", age: 25}
     ] })
   }
 
-  render() {
     return (
       <div className="App">
         <h1>Hi, I am a React App</h1>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
-        <Person name= {this.state.persons[0].name} age={this.state.persons[0].age}  />
-        <Person name= {this.state.persons[1].name} age={this.state.persons[1].age} >My Hobbies: Gaming</Person>
-        <Person name= {this.state.persons[2].name} age={this.state.persons[2].age} />
+        <button onClick={switchNameHandler}>Switch Name</button>
+        <Person name= {personState.persons[0].name} age={personState.persons[0].age}  />
+        <Person name= {personState.persons[1].name} age={personState.persons[1].age} >My Hobbies: Gaming</Person>
+        <Person name= {personState.persons[2].name} age={personState.persons[2].age} />
       </div>
       
     );
     //return React.createElement('div',{className: 'App'}, React.createElement('h1',null,'This is a React Component'));
-   }
+
 }
 
-export default App;
+export default app;
+
+
+
+
